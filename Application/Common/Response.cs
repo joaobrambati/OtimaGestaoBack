81435ab959cb98ae4eb302d626196ffca27ed95b
joaoBrambati <joao.brambati@litoraltextil.com.br>
2025-03-05 12:39:46 -0300

    Modelo Response e remoção de classes padrão após criar libs

---
+++ Application/Common/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common
+{
+    public class Response<T>
+    {
+        public T? Data { get; set; }
+        public bool Status { get; set; } 
+        public string Message { get; set; } = string.Empty;
+    }
+}
