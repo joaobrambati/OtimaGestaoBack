1d3957fe1f767657448f6294e96ca2fa8c06b427
joaoBrambati <joao.brambati@litoraltextil.com.br>
2025-03-05 13:07:47 -0300

    Instalação de pacotes

--- OtimaGestaoApi/obj/Debug/net9.0/OtimaGestaoApi.AssemblyInfo.cs
+++ OtimaGestaoApi/obj/Debug/net9.0/OtimaGestaoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtimaGestaoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4a7be119dd1bb94a3d0d4ad0fbe1f650104e6f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81435ab959cb98ae4eb302d626196ffca27ed95b")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtimaGestaoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtimaGestaoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
