4caab9c86bb17a5ce28370dc365d3628710aa4df
joaoBrambati <joao.brambati@litoraltextil.com.br>
2025-03-05 13:22:47 -0300

    Models de Cliente e Produto

---
+++ Domain/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
---
+++ Domain/Entities/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities
+{
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public int Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
